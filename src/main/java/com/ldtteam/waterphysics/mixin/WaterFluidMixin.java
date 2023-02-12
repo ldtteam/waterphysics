8b05c42f5f96dd560be5f9268988d3b0d21149a0
Raycoms <ray.neiheiser@gmail.com>
2023-02-12 10:43:32 +0100

    make oceans and river free

--- src/main/java/com/ldtteam/waterphysics/mixin/WaterFluidMixin.java
+++ src/main/java/com/ldtteam/waterphysics/mixin/WaterFluidMixin.java
@@ -1,26 +1,29 @@
 package com.ldtteam.waterphysics.mixin;
 
-import com.ldtteam.waterphysics.utils.Utils;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.level.material.Fluid;
-import net.minecraft.world.level.material.FluidState;
-import net.minecraft.world.level.material.Fluids;
-import net.minecraft.world.level.material.WaterFluid;
+import net.minecraft.core.Holder;
+import net.minecraft.world.level.LevelReader;
+import net.minecraft.world.level.biome.Biome;
+import net.minecraft.world.level.biome.Biomes;
+import net.minecraft.world.level.material.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WaterFluid.class)
-public abstract class WaterFluidMixin
+public abstract class WaterFluidMixin extends FlowingFluid
 {
-
     @Inject(method = "canConvertToSource", cancellable = true, at = @At("TAIL"))
-    public void neighborChanged(final CallbackInfoReturnable<Boolean> cir)
+    public void canConvertToSource(final CallbackInfoReturnable<Boolean> cir)
     {
         cir.setReturnValue(false);
+    }
+
+    @Override
+    public boolean canConvertToSource(FluidState state, LevelReader reader, BlockPos pos)
+    {
+        final Holder<Biome> biome = reader.getBiome(pos);
+        return biome.is(Biomes.OCEAN) || biome.is(Biomes.RIVER);
     }
 }
