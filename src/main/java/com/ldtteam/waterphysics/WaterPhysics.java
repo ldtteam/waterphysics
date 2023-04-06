5c83b70a56b984f217bb6abe20ba66a938aeab90
Raycoms <ray.neiheiser@gmail.com>
2023-04-06 19:27:28 +0200

    piston, bucket and fill fixes

--- src/main/java/com/ldtteam/waterphysics/WaterPhysics.java
+++ src/main/java/com/ldtteam/waterphysics/WaterPhysics.java
@@ -1,12 +1,19 @@
 package com.ldtteam.waterphysics;
 
 import com.ldtteam.waterphysics.handlers.*;
-import net.minecraftforge.event.level.LevelEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraftforge.event.entity.player.FillBucketEvent;
+import net.minecraftforge.event.level.PistonEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import static net.minecraft.world.level.block.Block.UPDATE_ALL;
 
 @Mod("waterphysics")
 public class WaterPhysics
@@ -20,14 +27,35 @@ public class WaterPhysics
     {
         Mod.EventBusSubscriber.Bus.FORGE.bus().get().register(this.getClass());
         ITransformationHandler.HANDLERS.add(new WaterFlow());
+        ITransformationHandler.HANDLERS.add(new WaterSourcing());
     }
 
     @SubscribeEvent
-    public static void LevelLoadEvent(LevelEvent.Load event)
+    public static void BucketPlaceEvent(final FillBucketEvent event)
     {
         if (event.getLevel().isClientSide())
         {
             return;
+        }
+
+        if (event.getEmptyBucket().getItem() == Items.WATER_BUCKET && event.getTarget() instanceof BlockHitResult)
+        {
+            event.getLevel().scheduleTick(new BlockPos(((BlockHitResult) event.getTarget()).getBlockPos()), Fluids.WATER, 120);
+        }
+    }
+
+    @SubscribeEvent
+    public static void PistonPushEvent(final PistonEvent event)
+    {
+        if (event.getLevel().isClientSide() || event.getPistonMoveType() != PistonEvent.PistonMoveType.EXTEND)
+        {
+            return;
+        }
+
+        final BlockState state = event.getLevel().getBlockState(event.getFaceOffsetPos());
+        if (state.getFluidState() != null && state.getFluidState().is(Fluids.WATER) && state.getFluidState().isSource())
+        {
+            event.getLevel().setBlock(event.getFaceOffsetPos().relative(event.getDirection()), state, UPDATE_ALL);
         }
     }
 }
---
+++ src/main/java/com/ldtteam/waterphysics/handlers/WaterSourcing.java
@@ -0,0 +1,51 @@
+package com.ldtteam.waterphysics.handlers;
+
+import com.ldtteam.waterphysics.utils.Utils;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.chunk.LevelChunk;
+import net.minecraft.world.level.chunk.LevelChunkSection;
+
+import static net.minecraft.world.level.block.Block.UPDATE_ALL;
+
+/**
+ * Three sides sources can convert to source.
+ */
+public class WaterSourcing implements ITransformationHandler
+{
+    @Override
+    public boolean transforms(final BlockState state)
+    {
+         return state.getBlock() == Blocks.WATER && !state.getFluidState().isSource() ;
+    }
+
+    @Override
+    public boolean ready(final long worldTick, final LevelChunk chunk)
+    {
+        return chunk.getLevel().isRaining();
+    }
+
+    @Override
+    public void transformBlock(final BlockPos relativePos, final LevelChunk chunk, final int chunkSection, final BlockState input)
+    {
+        final LevelChunkSection section = chunk.getSections()[chunkSection];
+        final BlockPos worldPos = Utils.getWorldPos(chunk, section, relativePos);
+
+        int waterCount = 0;
+        for (final Direction direction : Direction.Plane.HORIZONTAL)
+        {
+            final BlockState state = Utils.getBlockState(chunk, relativePos.relative(direction), chunkSection);
+            if (state.getFluidState() != null && state.getFluidState().isSource())
+            {
+                waterCount++;
+            }
+        }
+
+        if (waterCount >= 3)
+        {
+            chunk.getLevel().setBlock(worldPos, Blocks.WATER.defaultBlockState(), UPDATE_ALL);
+        }
+    }
+}
