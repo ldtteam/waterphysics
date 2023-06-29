38d726842904f6deb529a38b0efade6a670e71d0
Raycoms <ray.neiheiser@gmail.com>
2023-06-29 10:15:13 +0200

    fix

--- src/main/java/com/ldtteam/waterphysics/mixin/WaterNeighborMixin.java
+++ src/main/java/com/ldtteam/waterphysics/mixin/WaterNeighborMixin.java
@@ -3,6 +3,7 @@ package com.ldtteam.waterphysics.mixin;
 import com.ldtteam.waterphysics.utils.Utils;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.FluidState;
 import net.minecraft.world.level.material.Fluids;
@@ -22,7 +23,7 @@ public abstract class WaterNeighborMixin
     @Inject(method = "tick", at = @At("TAIL"))
     public void waterPhysicsNeighborChanged(final Level level, final BlockPos pos, final CallbackInfo ci)
     {
-        if (isSource() && getType() == Fluids.WATER && !Utils.tryMoveBelow(level, pos, false) && !Utils.tryMoveSidewards(level, pos, false))
+        if (isSource() && getType() == Fluids.WATER && level.getBlockState(pos).getBlock() == Blocks.WATER && !Utils.tryMoveBelow(level, pos, false) && !Utils.tryMoveSidewards(level, pos, false))
         {
             Utils.tryMoveDiagonally(level, pos, false);
         }
